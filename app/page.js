d1b21c78a96408008c7d0d2c90db2b01210d0bb7
snz-dsktop <snzmanuel02@gmail.com>
2024-04-02 17:48:00 -0300

    readme

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Roboto } from 'next/font/google'
+
+
 
 function page() {
   return (
-    <div className='container'>
-      <div className='header'>
-        <div className='window'></div>
+    <div>
+      <div className='container'>
+        <div className='header'>bash - SNZManu</div>
       </div>
     </div>
   )
